591e5c13aed8b6df55d89781caad858fb0b8d288
Saurabh Vyas <saurabhorange@msn.com>
2020-04-25 19:40:20 +0530

    minified it a bit more

--- data_utils.py
+++ data_utils.py
@@ -6,18 +6,12 @@ This script has common utility functions, and is used by main download.py script
 
 """
 
-
-
-
 import subprocess
 import logging
-
 import json
 import urllib.request, json 
 import urllib
-#from urlparse import urlparse
 from os.path import splitext
-
 import logging 
 import os.path
 import os
@@ -26,18 +20,10 @@ import _pickle as pickle
 
 
 current_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-
 conversion_file_set=set() # this basically stores all utterance ids of files already converted to wav
-
-
-
 words_set = set() # a set to store words for lexicon
-
-
-
-
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
 
 def setup_logger(name, log_file, level=logging.INFO):
     """To setup as many loggers as you want"""
@@ -51,15 +37,7 @@ def setup_logger(name, log_file, level=logging.INFO):
 
     return logger
 
-def hasNumbers(inputString):
-    """ checks if string has numbers or not"""
-    return any(char.isdigit() for char in inputString)
 
-
-
-
-
-    
 def init_system(language_code):
     """
     Basically does some initialization stuff like loading some files
@@ -83,9 +61,6 @@ def init_system(language_code):
     
     print(str(len(conversion_file_set)) + " files have already been converted to wav so will skip those for language " + language_code )
 
-
-
-
 def close_system(language_code):
     """
     Basically does some final post processing like storing stateof dictionary etc
@@ -94,11 +69,6 @@ def close_system(language_code):
 
     filepath="data/" + language_code + "/" + language_code + ".set"
     write_list_to_file(list(conversion_file_set),filepath)
-
-
-
-
-
 
 def generic_shell(shell_command,log_file_name):
     """
@@ -112,16 +82,10 @@ def generic_shell(shell_command,log_file_name):
                         stdout=subprocess.PIPE, 
                         stderr=subprocess.PIPE,shell=True)
         stdout, stderr = process.communicate()
-        #stdout, stderr
-        #print(stdout)
 
         if stderr:
-            #print(stderr)
-            #print("Exception during running generic shell with following command - ")
-            #print(shell_command)
-            shell_logger=setup_logger(log_file_name, log_file_name, level=logging.INFO)
-            #print(shell_command)
 
+            shell_logger=setup_logger(log_file_name, log_file_name, level=logging.INFO)
             shell_logger.error(stderr)
 
 
@@ -137,103 +101,19 @@ def count_lines(file_path):
         lines = len(foo.readlines())
     return lines
 
-def create_kaldi_subset(wav_scp_path,final_kaldi_dataset_dir,language_code,suffix_dir):
-
-
-    "creates a subset of data for train and test"
-    """
-    suffix_dir is something like ta_15000 , we need this to create subdirectories
-    we assume the split directory like kaldi_outputs/ta/ta_15k has already been created
-
-    """
-    print("creating train and test split")
-
-    shell_command1="awk '{ print $1 }' " + wav_scp_path + " > dataset_ids"
-    generic_shell(shell_command1,"logs/" + language_code + "." + "subset.log")
-
-    lines_dataset=count_lines("./dataset_ids")
-    print("total rows in dataset : " + str(lines_dataset))
-    test_lines=int(0.1 * lines_dataset)
-    print("total rows in testset : " + str(test_lines))
-    
-    subset_choice=input("How would you like to create test split ? choose 1 for default sentence based split , if not enter 2 please create file test_ids in same folder (you can use the generated dataset_ids file)")
-    
-    if subset_choice=="1":
-        print("by default usng 10 percent of overall data for test set")
-        shell_command2="shuf -n " + str(test_lines) + " dataset_ids > test_ids  "
-
-    elif subset_choice=="2":
-        print("using users test_ids file for splitting")
-
-
-   
-    
-
-    
-    shell_command3="cat dataset_ids | grep -v -f test_ids > train_ids"
-
-    shell_command4="cat kaldi_outputs/wav.scp | grep  -f train_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/train/wav.scp"
-    shell_command5="cat kaldi_outputs/text | grep  -f train_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/train/text"
-    shell_command6="cat kaldi_outputs/spk2utt | grep  -f train_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/train/spk2utt"
-
-
-    shell_command7="cat kaldi_outputs/wav.scp | grep  -f test_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/test/wav.scp"
-    shell_command8="cat kaldi_outputs/text | grep  -f test_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/test/text"
-    shell_command9="cat kaldi_outputs/spk2utt | grep  -f test_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/test/spk2utt"
-
-    shell_command10="cat kaldi_outputs/utt2spk | grep  -f train_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/train/utt2spk"
-    shell_command11="cat kaldi_outputs/utt2spk | grep  -f test_ids > kaldi_outputs/" + language_code + "/" + suffix_dir + "/data/test/utt2spk"
-
-
-
-    generic_shell(shell_command2,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command3,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command4,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command5,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command6,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command7,"logs/" + language_code + "." + "ssubset.log")
-    generic_shell(shell_command8,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command9,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command10,"logs/" + language_code + "." + "subset.log")
-    generic_shell(shell_command11,"logs/" + language_code + "." + "subset.log")
-    ####shuf -n 100 $audio_dir/wav.list > $audio_dir/test_wav.list                                                                                        
-    ####cat $audio_dir/wav.list | grep -v -f $audio_dir/test_wav.list > $audio_dir/train_wav.list 
-
-    #utils/subset_data_dir.sh --utt-list
-
-    #generic_shell("","logs/subset.log")
-
 
 def rm_unnecessary_files(language_code):
     """ this functions deletes some temporary files , for example before train/test split """
-    #generic_shell("rm  kaldi_outputs/wav.scp","logs/" + language_code + "." + "rm.log")
-    #generic_shell("rm  kaldi_outputs/text","logs/" + language_code + "." + "rm.log")
-    #generic_shell("rm  kaldi_outputs/spk2utt","logs/" + language_code + "." + "rm.log")
-    #generic_shell("rm  lexicon_left","logs/" + language_code + "." + "rm.log")
-    #generic_shell("rm  lexicon.txt","logs/" + language_code + "." + "rm.log")
-    #generic_shell("rm  wav.list","logs/" + language_code + "." + "rm.log")
 
     files_to_remove=['lexicon_left','lexicon.txt','wav.list','test_ids','dataset_ids','train_ids']
     for file in files_to_remove:
         remove_file(file)
-
-    #remove_file("kaldi_outputs/wav.scp")
-    #remove_file("kaldi_outputs/text")
-    #remove_file("kaldi_outputs/spk2utt")
-    #remove_file("kaldi_outputs/utt2spk")
-    #remove_file("lexicon_left")
-    #remove_file("lexicon.txt")
-    #remove_file("wav.list")
-    #remove_file("test_ids")
-    #remove_file("dataset_ids")
-    #remove_file("train_ids")
 
 
 def write_list_to_file(my_list,filepath):
     """
     a simple utility function to write a python list to file 
     """
-
     with open(filepath, mode='wt', encoding='utf-8') as myfile:
         myfile.write('\n'.join(my_list))
 
@@ -241,45 +121,26 @@ def read_file_to_list(filepath):
     """
         simple utility function which basically reads input file having one entry in each line
         to a python list 
-
     """
     if os.path.isfile(filepath): 
-
         with open(filepath) as f:
             return f.read().splitlines() 
     else:
         return []
 
+def create_dir(dir_path,language_code):
+    if not os.path.isdir(dir_path):
+        generic_shell("mkdir " + dir_path ,"logs/" + language_code + "." + "mkdir.log")
 
 
 def create_kaldi_directories(language_code,destination_wav_dir,create_subset_split_dirs=False):
     """ this function generates folder structure which kaldi expects, also creates some general directories not for kaldi
-         
     """
-
     wav_scp_count=0
-
-    # kaldi specific
-    #generic_shell("rm -rf kaldi_outputs","logs/rm.log")
-
-    if not os.path.isdir("logs"):
-        generic_shell("mkdir logs" ,"logs/" + language_code + "." + "mkdir.log")
-
-    # check if kaldi_outputs exist
-    if not os.path.isdir("data"):
-        generic_shell("mkdir data" ,"logs/" + language_code + "." + "mkdir.log")
-
-    if not os.path.isdir("data/" + language_code):
-        generic_shell("mkdir data/" + language_code ,"logs/" + language_code + "." + "mkdir.log")
-
-
-    if not os.path.isdir("kaldi_outputs"):
-        generic_shell("mkdir kaldi_outputs" ,"logs/" + language_code + "." + "mkdir.log")
-    
-    if not os.path.isdir("kaldi_outputs/" + language_code ):
-            generic_shell("mkdir kaldi_outputs/"  +  language_code ,"logs/" + language_code + "." + "mkdir.log")
-
-
+    mkdir_dirs=["logs","data","data/" + language_code , "kaldi_outputs" , "kaldi_outputs/" + language_code]
+    for dir in mkdir_dirs:
+        create_dir(dir,language_code)
+        
 
     if create_subset_split_dirs==True:
 
@@ -287,8 +148,6 @@ def create_kaldi_directories(language_code,destination_wav_dir,create_subset_spl
             # this means all files were already processed so there is no new file
             print("not creating new split since no new file present")
             return
-
-        
         
         # read the length of wav.scp in kaldi_outputs/language_id
         wav_scp_count=str(count_lines("kaldi_outputs/"  + "wav.scp"))
@@ -300,8 +159,6 @@ def create_kaldi_directories(language_code,destination_wav_dir,create_subset_spl
             print ("subset file exists for language")
             print("wav scp count : ")
             print(wav_scp_count)
-
-
 
             subset_counts=read_file_to_list("kaldi_outputs/" +  language_code + "/.subsets.txt")
             
@@ -316,27 +173,14 @@ def create_kaldi_directories(language_code,destination_wav_dir,create_subset_spl
                 shell_command4="cp kaldi_outputs/utt2spk kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count
                 
                 shell_command5="cp data/" + language_code +  "/lexicon.txt kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count
-                generic_shell(shell_command,"logs/" + language_code + "." + "cp.log")
-                generic_shell(shell_command2,"logs/" + language_code + "." + "cp.log")
-                generic_shell(shell_command3,"logs/" + language_code + "." + "cp.log")
-                generic_shell(shell_command4,"logs/" + language_code + "." + "cp.log")
-                generic_shell(shell_command5,"logs/" + language_code + "." + "cp.log")
+
+                cp_shell_commands=[shell_command,shell_command2,shell_command3,shell_command4,shell_command5]
+                for command in cp_shell_commands:
+                    generic_shell(command, "logs/" + language_code + "." + "cp.log" )
+
 
                 generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/local","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +   language_code + "/" + language_code + "_" + wav_scp_count + "/data/local/dict","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/train","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count +  "/data/test","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/exp","logs/" + language_code + "." + "mkdir.lsog")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/mfcc","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/local/data","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count +  "/data/local/lm_temp","logs/" + language_code + "." + "mkdir.log")
-                generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/local/kaldi_lm","logs/" + language_code + "." + "mkdir.log")
-
-
-                
-
-
+        
 
         else:
             print ("subsets.txt doesnt exist creating it for the first time")
@@ -355,22 +199,10 @@ def create_kaldi_directories(language_code,destination_wav_dir,create_subset_spl
             generic_shell(shell_command4,"logs/" + language_code + "." + "cp.log")
 
             generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/local","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +   language_code + "/" + language_code + "_" + wav_scp_count + "/data/local/dict","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/train","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count +  "/data/test","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/exp","logs/" + language_code + "." + "mkdir.lsog")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/mfcc","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/local/data","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count +  "/data/local/lm_temp","logs/" + language_code + "." + "mkdir.log")
-            #generic_shell("mkdir kaldi_outputs/" +  language_code + "/" + language_code + "_" + wav_scp_count + "/data/local/kaldi_lm","logs/" + language_code + "." + "mkdir.log")
 
-    # non kaldi
-    #generic_shell("rm -rf logs","logs/rm.log")
     if not os.path.isdir("logs"):
         generic_shell("mkdir logs","logs/" + language_code + "." + "mkdir.log")
-    #generic_shell("rm -rf wavs","logs/rm.log")
-    #generic_shell("rm -rf audios","logs/" + language_code + "." + "rm.log")
+
     if not os.path.isdir(destination_wav_dir):
         generic_shell("mkdir " + destination_wav_dir ,"logs/" + language_code + "." + "mkdir.log")
     #generic_shell("mkdir audios","logs/" + language_code + "." + "mkdir.log")
@@ -380,16 +212,9 @@ def create_kaldi_directories(language_code,destination_wav_dir,create_subset_spl
     return language_code + "_" + str(wav_scp_count) # this will be used by other functions later, to store files in this subset
 
 
-    
-#from datetime import datetime
-
-#timestamp = 1545730073
-#dt_object = datetime.fromtimestamp(timestamp)
 
 def read_transcription(transcription_id,transcription_filepath):
-    # gets transcription from transcriptions.txt having corresponding id
-    
-    
+    # gets transcription from transcriptions.txt having corresponding id  
     import csv
     transcription_id=transcription_id.replace(".wav","")
     with open(transcription_filepath) as inf:
@@ -398,7 +223,6 @@ def read_transcription(transcription_id,transcription_filepath):
             if row[0]==transcription_id:
                 return ' '.join(row[1:])
 
-    
     return ""
 
 def append_row_file(file,row):
@@ -426,20 +250,13 @@ def create_wav_list_file(wav_file_path,wav_list_path,wav_scp_path):
 
 def create_text_file(wav_file_path,text_file_path,transcription_filepath):
     """
-
-    appends to kaldi text ( data/text ) file 
-    
-
+    sappends to kaldi text ( data/text ) file 
     """
 
     sentence_id=wav_file_path.split("_")[2].split('.')[0]
-
     transcription=read_transcription(sentence_id,transcription_filepath)
-
     text_line=wav_file_path.split("/")[-1].replace(".wav","") + " " +  transcription
-
     append_row_file(text_file_path,text_line)
-
 
 
 def check_if_file_exists(filepath):
@@ -466,8 +283,6 @@ def g2p_create_lexicon(input_lexicon_file,output_lexicon_file,lexicon_language_c
 
      ~/nv-g2p/rule/lexicon_post_process.sh "hindi" ~/datasets/rich_transcription/hin_regional/lexicon_input.txt ~/datasets/rich_transcription/hin_regi\
 onal/lexicon_final.txt
-
-
     '''
 
     print("Running G2p and creating final lexicon file")
@@ -540,8 +355,6 @@ def remove_file(filepath):
 
     if os.path.isfile(filepath):
         os.remove(filepath)
-
-
         
 def convert_mp3_to_wav(mp3_path,output_wav_dir,language_code):
 
@@ -554,8 +367,6 @@ def convert_mp3_to_wav(mp3_path,output_wav_dir,language_code):
     #/usr/bin/ffmpeg -i mp3_dir/$file wav_dir/${out_file}_tmp.wav; 
     #sox wav_dir/${out_file}_tmp.wav -c1 -r16000 -b16 wav_dir/${out_file}.wav ;
 
-
-    #print("converting file " + mp3_path )
     out_file_temp=  mp3_path.split("/")[-1].replace(".mp3",".temp.wav")
     out_file=  mp3_path.split("/")[-1].replace(".mp3",".wav")
 
@@ -566,23 +377,19 @@ def convert_mp3_to_wav(mp3_path,output_wav_dir,language_code):
     #print(stdout, stderr)
 
     if stderr:
-        #ssprint("error during ffmpeg ")
-        #logging.error(stdout)
+
         logging.error(stderr)
 
     process2 = subprocess.Popen(['sox', output_wav_dir + out_file_temp , '-c1' , '-r16000' , '-b16',output_wav_dir + out_file]
                      ,stdout=subprocess.PIPE, 
                      stderr=subprocess.PIPE)
     stdout2, stderr2 = process2.communicate()
-    #print(stdout2, stderr2)
 
-    # remove temp wav file
     shell_command="rm " + output_wav_dir + out_file_temp 
     generic_shell(shell_command,"logs/" + language_code + "." + "rm.log")
 
     if stderr2:
-        #print("error during sox ")
-        #logging.error(stdout)
+
         logging.error(stderr2)
 
 
@@ -616,16 +423,10 @@ def download_audio_json(final_audio_url,destination_audio_file):
 
 def convert_single_file(url,downloaded_audio_count,destination_directory,speaker_id,source_mp3_directory,destination_wav_directory,text_filepath,spk2utt_filepath,utt2spk_filepath,transcription_filepath,wav_list_file,wav_scp_path,language_code):
     """
-    downloads mp3 file
-    converts mp3 to wav file
-    updates wav.list file
-    updates wav.scp file
-    updates kaldi_outputs/text file
-    updates spk2utt file
+    converts mp3 to wav file , updates wav.list , wav.scp text ,spk2utt file
     """
     try:
 
-        
         destination_filename= url.split("/")[-1]
 
         destination_path= source_mp3_directory + destination_filename 
